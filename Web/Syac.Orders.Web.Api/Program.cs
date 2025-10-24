7b5b33db98f2282921110769b3cc274882a21794
Rafael Cascante <rgcascante@outlook.com>
2025-10-24 11:52:24 -0500

    Fix: Cors

--- Web/Syac.Orders.Web.Api/Program.cs
+++ Web/Syac.Orders.Web.Api/Program.cs
@@ -24,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseCors(e => e.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
